fa32642321348c9ea01bfbf682f7cf7c4b25ce72
DYGV <11eisuke88@gmail.com>
2022-08-09 01:29:26 +0900

    replace tabs with spaces

--- hls/hls_fft.h
+++ hls/hls_fft.h
@@ -13,21 +13,19 @@ const unsigned FFT_LENGTH_MAX = 1 << TWO_TO_THE_POWER_OF_N_MAX;
 
 /// @see https://docs.xilinx.com/r/2021.1-English/ug1399-vitis-hls/FFT-Struct-Parameters
 struct fft_param: hls::ip_fft::params_t {
-	static const bool has_nfft = true;
-	static const bool ovflo = false;
-	static const unsigned input_width = 32;
-	static const unsigned output_width = 32;
-	static const unsigned status_width = 8;
-	static const unsigned config_width = 24;
-	static const unsigned phase_factor_width = 24;
-	static const unsigned stages_block_ram =
-			(TWO_TO_THE_POWER_OF_N_MAX < 10) ?
-					0 : (TWO_TO_THE_POWER_OF_N_MAX - 9);
-	static const unsigned max_nfft = TWO_TO_THE_POWER_OF_N_MAX;
-	static const unsigned arch_opt = hls::ip_fft::pipelined_streaming_io;
-	static const unsigned ordering_opt = hls::ip_fft::natural_order;
-	static const unsigned scaling_opt = hls::ip_fft::scaled;
-	static const unsigned rounding_opt = hls::ip_fft::truncation;
+    static const bool has_nfft = true;
+    static const bool ovflo = false;
+    static const unsigned input_width = 32;
+    static const unsigned output_width = 32;
+    static const unsigned status_width = 8;
+    static const unsigned config_width = 24;
+    static const unsigned phase_factor_width = 24;
+    static const unsigned stages_block_ram = (TWO_TO_THE_POWER_OF_N_MAX < 10) ? 0 : (TWO_TO_THE_POWER_OF_N_MAX - 9);
+    static const unsigned max_nfft = TWO_TO_THE_POWER_OF_N_MAX;
+    static const unsigned arch_opt = hls::ip_fft::pipelined_streaming_io;
+    static const unsigned ordering_opt = hls::ip_fft::natural_order;
+    static const unsigned scaling_opt = hls::ip_fft::scaled;
+    static const unsigned rounding_opt = hls::ip_fft::truncation;
 };
 
 typedef hls::ip_fft::config_t<fft_param> config_t;
@@ -40,5 +38,4 @@ typedef hls::axis<std::complex<float>, 0, 0, 0> data_t;
 typedef hls::stream<data_t> fft_stream;
 
 // Top function
-void hls_fft(fft_stream &stream_input, fft_stream &stream_output,
-		unsigned size);
+void hls_fft(fft_stream &stream_input, fft_stream &stream_output, unsigned size);
--- hls/hls_fft_tb.cpp
+++ hls/hls_fft_tb.cpp
@@ -4,33 +4,33 @@
 #include "hls_fft.h"
 
 int main() {
-	// Preparation of FFT sample data
-	std::fstream fin("sim_sample.dat");
-	std::vector<float> vec_data;
-	float num;
-	int nfft = 10;
-	int data_size = 1 << nfft; // Perform 32-point FFT
-	for (int i = 0; i < data_size; i++) {
-		fin >> num;
-		vec_data.push_back(num);
-	}
+    // Preparation of FFT sample data
+    std::fstream fin("sim_sample.dat");
+    std::vector<float> vec_data;
+    float num;
+    int nfft = 10;
+    int data_size = 1 << nfft; // Perform 32-point FFT
+    for (int i = 0; i < data_size; i++) {
+        fin >> num;
+        vec_data.push_back(num);
+    }
 
-	// Writing FFT sample data to the stream
-	fft_stream input_data, output_data;
-	for (int i = 0; i < data_size; i++) {
-		data_t in_data;
-		in_data.data = vec_data[i];
-		in_data.last = (i == data_size - 1);
-		input_data.write(in_data);
-	}
+    // Writing FFT sample data to the stream
+    fft_stream input_data, output_data;
+    for (int i = 0; i < data_size; i++) {
+        data_t in_data;
+        in_data.data = vec_data[i];
+        in_data.last = (i == data_size - 1);
+        input_data.write(in_data);
+    }
 
-	// FFT Execution
-	hls_fft(input_data, output_data, nfft);
+    // FFT Execution
+    hls_fft(input_data, output_data, nfft);
 
-	// Reading FFT results from a stream
-	data_t dout;
-	do {
-		dout = output_data.read();
-		std::cout << dout.data << ", ";
-	} while (!dout.last);
+    // Reading FFT results from a stream
+    data_t dout;
+    do {
+        dout = output_data.read();
+        std::cout << dout.data << ", ";
+    } while (!dout.last);
 }
